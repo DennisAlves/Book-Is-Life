0df3e9b6c131a663ff3f7c1f87484db2a903f707
DennisAlves <dennis.alves@outlook.com>
2020-12-10 19:00:42 -0300

    implementação de api para preenchimento do endereço apartir do cep

--- book-is-life-front-end/src/containers/App/index.js
+++ book-is-life-front-end/src/containers/App/index.js
@@ -1,44 +1,46 @@
 import React from "react";
-import { Provider } from "react-redux";
+import {Provider} from "react-redux";
 import thunk from "redux-thunk";
-import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
+import {CssBaseline, MuiThemeProvider} from "@material-ui/core";
 import Router from "../Router";
-import { createBrowserHistory } from "history";
-import { createStore, applyMiddleware, compose } from "redux";
-import { generateReducers } from "../../reducers";
-import { routerMiddleware } from "connected-react-router";
-import { createMuiTheme } from '@material-ui/core/styles';
+import {createBrowserHistory} from "history";
+import {applyMiddleware, compose, createStore} from "redux";
+import {generateReducers} from "../../reducers";
+import {routerMiddleware} from "connected-react-router";
+import {createMuiTheme} from '@material-ui/core/styles';
 import purple from '@material-ui/core/colors/purple';
 //import orange from '@material-ui/core/colors/orange';
+
 
 export const history = createBrowserHistory();
 
 const theme = createMuiTheme({
-  palette: {
-    primary: purple,
-    secondary: { main: '#9c27b0'},
-  },
-  status: {
-    danger: 'red',
-  },
-  });
+    palette: {
+        primary: purple,
+        secondary: {main: '#9c27b0'},
+    },
+    status: {
+        danger: 'red',
+    },
+});
 
 const middlewares = [
-  applyMiddleware(routerMiddleware(history), thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__
-    ? window.__REDUX_DEVTOOLS_EXTENSION__()
-    : f => f
+    applyMiddleware(routerMiddleware(history), thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+        ? window.__REDUX_DEVTOOLS_EXTENSION__()
+        : f => f
 ];
 
 const store = createStore(generateReducers(history), compose(...middlewares));
 
 export const App = () => (
-  <Provider store={store}>
-    <MuiThemeProvider theme={theme}>
-      <CssBaseline />
-      <Router history={history} />
-    </MuiThemeProvider>
-  </Provider>
+
+    <Provider store={store}>
+        <MuiThemeProvider theme={theme}>
+            <CssBaseline/>
+            <Router history={history}/>
+        </MuiThemeProvider>
+    </Provider>
 );
 
 export default App;
